b1fd83bd5f76148b8abdbd8b944da9b419da970c
Shamrik Dmitriy <shamrik.dmitriy@gmail.com>
2019-09-01 17:23:02 +0600

    Create class Point

---
+++ Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Point
+    {
+        #region Public Properties
+
+        /// <summary>
+        ///     Координата по оси X
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        ///     Координата по оси Y
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        ///     Символ вывода
+        /// </summary>
+        public char Symbol { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Point(int x, int y, char symbol)
+        {
+            X = x;
+            Y = y;
+            Symbol = symbol;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        ///     Отобразит символ в заданных координатах
+        /// </summary>
+        public void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(Symbol);
+        }
+
+        #endregion
+    }
+}
