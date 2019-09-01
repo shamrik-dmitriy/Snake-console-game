2284871abab38945a04fc8dbe73008d09172abe8
Shamrik Dmitriy <shamrik.dmitriy@gmail.com>
2019-09-01 17:49:15 +0600

    Create class horizontal line

---
+++ HorizontalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class HorizontalLine
+    {
+        private List<Point> LinePoints { get; set; }
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symbol)
+        {
+            for (int i = xLeft; i <= xRight; i++)
+            {
+                LinePoints.Add(new Point(i, y, symbol));
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (var linePoint in LinePoints)
+            {
+                
+            }
+        }
+    }
+}
